be0e635a08f0135ed20c2c09dacfdac6a589e6cc
RoshanZedexInfo <roshan.moolya@zedexinfo.com>
2022-09-09 04:24:09 +0530

    first commit

---
+++ brian-tab.js
@@ -0,0 +1,55 @@
+
+const triggerBrianTab = (el)=>{
+    const target = el.currentTarget ? el.currentTarget : el;
+    const byATag = $(target).attr("id");
+    const byRadio = $(target).attr('for');
+    let id = "";
+    if(byATag){
+        id = '#' + byATag;
+    }
+    if(byRadio){
+        id = '#' + byRadio.replace('-radio',"");
+
+    }
+    if(id){
+        $('.tabbuttonwrapper .tab-button').removeClass('w--current');
+        $('.w-tab-pane').removeClass('w--tab-active');
+    
+        $(id).addClass('w--current');
+        $(id+"-content").addClass('w--tab-active');
+        if(byATag){
+            $(id+"-radio").prop("checked", true);
+        }
+        replaceUrl(id);
+    }
+}
+
+function replaceUrl(id=""){
+    let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + id;
+    window.history.pushState({ path: newurl }, '', newurl);
+}
+
+const triggerBrianOnLoad = ()=>{
+let allTabBtns = document.querySelectorAll('.tabbuttonwrapper .tab-button');
+let url = window.location.toString();
+let hash2 = window.location.hash.substring(1);
+if (hash2) {
+$('.tabbuttonwrapper .tab-button').removeClass('w--current');
+allTabBtns.forEach(function (el) {
+    let sqpl_link = '#' + el.getAttribute('id');
+    if (sqpl_link === window.location.hash) {
+        el.classList.add('w--current');
+        triggerBrianTab(el);
+    }
+})
+}
+}
+
+(function(){
+    function activateBrianTab(){
+        triggerBrianOnLoad()
+        $('.tabbuttonwrapper .tab-button').click(triggerBrianTab);
+        $('.w-tab-content .tab-label').click(triggerBrianTab);
+    }
+    window.addEventListener('load', activateBrianTab);
+})()
